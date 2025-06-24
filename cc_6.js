f74a1b77ad2a0ee0e54a5482246bf00f8c9e66ff
Mahala444 <jmj42@usf.edu>
2025-06-24 17:19:09 -0500

    Created Employees and Managers

--- cc_6.js
+++ cc_6.js
@@ -35,3 +35,9 @@ class Company {
     });
   }
 }
+
+const emp1 = new Employee("Alexander", "Engineering");
+const emp2 = new Employee("Brandon", "Marketing");
+const emp3 = new Employee("Catherine", "Sales");
+const mgr1 = new Manager("Rain", "Engineering", 5);
+const mgr2 = new Manager("Daniella", "Sales", 3);
