6e04038ac1151282949fe61207089d8e9003f55d
Mahala444 <jmj42@usf.edu>
2025-06-24 17:14:54 -0500

    Manager Class (Inherits from Employee)

--- cc_6.js
+++ cc_6.js
@@ -8,3 +8,14 @@ class Employee {
     return `Employee: ${this.name}, Department: ${this.department}`;
   }
 }
+
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department); // inherit name & department
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `Manager: ${this.name}, Department: ${this.department}, Team Size: ${this.teamSize}`;
+  }
+}
